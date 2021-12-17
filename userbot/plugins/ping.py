c98010fb3086f4f354a7008b73d3c59800a376ad
Deepjit Das <87997952+deepaiims@users.noreply.github.com>
2021-12-17 14:36:42 +0530

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -1,10 +1,55 @@
+# ==================================================================================================
+
+# Modified by https://t.me/deepaiims
+
+# It is simillar to my other plugin 'pping' (ping with media)
+
+# This randomly chooses from the given media links, i.e 'multi-pping', in short 'mping'
+
 import asyncio
+import os
+import random
 from datetime import datetime
 
+from userbot import catub
+
 from ..core.managers import edit_or_reply
-from . import catub, hmention
+from . import catub, hmention, reply_id
 
 plugin_category = "tools"
+
+# =========Some integrated custom vars============
+
+# add space b/w each telegraph link
+
+PING_PICS = (
+    os.environ.get("PING_PICS")
+    or "https://telegra.ph/file/1328d62db93ad22b69ba2.jpg https://telegra.ph/file/b2da6e4c55dd29600e4ed.jpg"
+)
+
+PING_PICS = PING_PICS.rsplit(" ")
+
+# Pre text i.e. before calculation ping
+
+PING_TEXT = os.environ.get("PING_TEXT") or "𝕾𝖙𝖆𝖗𝖙𝖎𝖓𝖌 𝖙𝖍𝖊 𝖌𝖆𝖒𝖊!!"
+
+# Post text i.e. the final message
+
+PONG_TEXT = os.environ.get("PONG_TEXT") or "𝕹𝖔𝖜 𝖑𝖊𝖙𝖘 𝖙𝖍𝖊 𝖌𝖆𝖒𝖊 𝖇𝖊𝖌𝖎𝖓!!"
+
+# Custom mention line
+
+PING_MENTION = os.environ.get("PING_MENTION") or "𝕽𝖚𝖑𝖊𝖘 𝕭𝖞"
+
+# Text after the ping value
+
+PING_PARTNER = os.environ.get("PING_PARTNER") or "𝖒𝖘"
+
+# average ping header
+
+AVG_TEXT = os.environ.get("AVG_TEXT") or "𝕬𝖛𝖊𝖗𝖆𝖌𝖊 𝖕𝖔𝖓𝖌!"
+
+# ================================================
 
 
 @catub.cat_cmd(
@@ -17,25 +62,46 @@ plugin_category = "tools"
     },
 )
 async def _(event):
+
     "To check ping"
+
     flag = event.pattern_match.group(1)
+
     start = datetime.now()
+
     if flag == " -a":
+
         catevent = await edit_or_reply(event, "`!....`")
+
         await asyncio.sleep(0.3)
+
         await catevent.edit("`..!..`")
+
         await asyncio.sleep(0.3)
+
         await catevent.edit("`....!`")
+
         end = datetime.now()
+
         tms = (end - start).microseconds / 1000
+
         ms = round((tms - 0.6) / 3, 3)
-        await catevent.edit(f"**☞ Average Pong!**\n➥ {ms} ms")
-    else:
-        catevent = await edit_or_reply(event, "<b><i>☞ Pong!</b></i>", "html")
-        end = datetime.now()
-        ms = (end - start).microseconds / 1000
+
         await catevent.edit(
-            f"<b><i>☞ Pong</b></i>\n➥ {ms} <b><i>ms\n➥ Bot of {hmention}</b></i>",
+            f"{AVG_TEXT}\n{ms} {PING_PARTNER}\n{PING_MENTION} {hmention}",
+            parse_mode="html",
+        )
+
+    else:
+
+        catevent = await edit_or_reply(event, f"{PING_TEXT}", "html")
+
+        end = datetime.now()
+
+        ms = (end - start).microseconds / 1000
+
+        await catevent.edit(
+            f"{PONG_TEXT}\n{ms} {PING_PARTNER}\n{PING_MENTION} {hmention}",
             parse_mode="html",
         )
 
@@ -46,11 +112,17 @@ async def _(event):
     info={"header": "Shows the server ping with extra animation", "usage": "{tr}fping"},
 )
 async def _(event):
+
     "To check ping with animation"
+
     start = datetime.now()
+
     animation_interval = 0.3
+
     animation_ttl = range(26)
+
     event = await edit_or_reply(event, "ping....")
+
     animation_chars = [
         "⬛⬛⬛⬛⬛⬛⬛⬛⬛",
         "⬛⬛⬛⬛⬛⬛⬛⬛⬛ \n⬛‎📶‎📶‎📶‎📶‎📶‎📶‎📶⬛",
@@ -79,11 +151,83 @@ async def _(event):
         "⬛⬛⬛⬛⬛⬛⬛⬛⬛ \n⬛‎📶‎📶‎📶‎📶‎📶‎📶‎📶⬛ \n⬛⬛⬛⬛‎📶⬛⬛‎📶⬛ \n⬛⬛⬛⬛‎📶⬛⬛‎📶⬛ \n⬛⬛⬛⬛‎📶⬛⬛‎📶⬛ \n⬛⬛⬛⬛⬛‎📶‎📶⬛⬛ \n⬛⬛⬛⬛⬛⬛⬛⬛⬛ \n⬛‎📶⬛⬛⬛⬛⬛‎📶⬛ \n⬛‎📶‎📶‎📶‎📶‎📶‎📶‎📶⬛ \n⬛‎📶⬛⬛⬛⬛⬛‎📶⬛ \n⬛⬛⬛⬛⬛⬛⬛⬛⬛ \n⬛‎📶‎📶‎📶‎📶‎📶‎📶‎📶⬛ \n⬛⬛⬛⬛⬛⬛‎📶⬛⬛ \n⬛⬛⬛⬛⬛‎📶⬛⬛⬛ \n⬛⬛⬛⬛‎📶⬛⬛⬛⬛ \n⬛‎📶‎📶‎📶‎📶‎📶‎📶‎📶⬛ \n⬛⬛⬛⬛⬛⬛⬛⬛⬛ \n⬛⬛‎📶‎📶‎📶‎📶‎📶⬛⬛ \n⬛‎📶⬛⬛⬛⬛⬛‎📶⬛ \n⬛‎📶⬛⬛⬛⬛⬛‎📶⬛ \n⬛‎📶⬛‎📶⬛⬛⬛‎📶⬛ \n⬛⬛‎📶‎📶⬛⬛‎📶⬛⬛ \n⬛⬛⬛⬛⬛⬛⬛⬛⬛ \n⬛‎📶⬛‎📶‎📶‎📶‎📶‎📶⬛ \n⬛⬛⬛⬛⬛⬛⬛⬛⬛",
         "⬛⬛⬛⬛⬛⬛⬛⬛⬛ \n⬛‎📶‎📶‎📶‎📶‎📶‎📶‎📶⬛ \n⬛⬛⬛⬛‎📶⬛⬛‎📶⬛ \n⬛⬛⬛⬛‎📶⬛⬛‎📶⬛ \n⬛⬛⬛⬛‎📶⬛⬛‎📶⬛ \n⬛⬛⬛⬛⬛‎📶‎📶⬛⬛ \n⬛⬛⬛⬛⬛⬛⬛⬛⬛ \n⬛‎📶⬛⬛⬛⬛⬛‎📶⬛ \n⬛‎📶‎📶‎📶‎📶‎📶‎📶‎📶⬛ \n⬛‎📶⬛⬛⬛⬛⬛‎📶⬛ \n⬛⬛⬛⬛⬛⬛⬛⬛⬛ \n⬛‎📶‎📶‎📶‎📶‎📶‎📶‎📶⬛ \n⬛⬛⬛⬛⬛⬛‎📶⬛⬛ \n⬛⬛⬛⬛⬛‎📶⬛⬛⬛ \n⬛⬛⬛⬛‎📶⬛⬛⬛⬛ \n⬛‎📶‎📶‎📶‎📶‎📶‎📶‎📶⬛ \n⬛⬛⬛⬛⬛⬛⬛⬛⬛ \n⬛⬛‎📶‎📶‎📶‎📶‎📶⬛⬛ \n⬛‎📶⬛⬛⬛⬛⬛‎📶⬛ \n⬛‎📶⬛⬛⬛⬛⬛‎📶⬛ \n⬛‎📶⬛‎📶⬛⬛⬛‎📶⬛ \n⬛⬛‎📶‎📶⬛⬛‎📶⬛⬛ \n⬛⬛⬛⬛⬛⬛⬛⬛⬛ \n⬛‎📶⬛‎📶‎📶‎📶‎📶‎📶⬛ \n⬛⬛⬛⬛⬛⬛⬛⬛⬛ \n \n My 🇵 🇮 🇳 🇬  Is : Calculating...",
     ]
+
     for i in animation_ttl:
+
         await asyncio.sleep(animation_interval)
+
         await event.edit(animation_chars[i % 26])
+
     end = datetime.now()
+
     ms = (end - start).microseconds / 1000
+
     await event.edit(
         f"‎‎‎‎‎‎‎‎‎⬛⬛⬛⬛⬛⬛⬛⬛⬛\n⬛📶📶📶📶📶📶📶⬛\n⬛⬛⬛⬛📶⬛⬛📶⬛\n⬛⬛⬛⬛📶⬛⬛📶⬛\n⬛⬛⬛⬛📶⬛⬛📶⬛\n⬛⬛⬛⬛⬛📶📶⬛⬛\n⬛⬛⬛⬛⬛⬛⬛⬛⬛\n⬛⬛📶📶📶📶📶⬛⬛\n⬛📶⬛⬛⬛⬛⬛📶⬛\n⬛📶⬛⬛⬛⬛⬛📶⬛\n⬛📶⬛⬛⬛⬛⬛📶⬛\n⬛⬛📶📶📶📶📶⬛⬛\n⬛⬛⬛⬛⬛⬛⬛⬛⬛\n⬛📶📶📶📶📶📶📶⬛\n⬛⬛⬛⬛⬛⬛📶⬛⬛\n⬛⬛⬛⬛⬛📶⬛⬛⬛\n⬛⬛⬛⬛📶⬛⬛⬛⬛\n⬛📶📶📶📶📶📶📶⬛\n⬛⬛⬛⬛⬛⬛⬛⬛⬛\n⬛⬛📶📶📶📶📶⬛⬛\n⬛📶⬛⬛⬛⬛⬛📶⬛\n⬛📶⬛⬛⬛⬛⬛📶⬛\n⬛📶⬛📶⬛⬛⬛📶⬛\n⬛⬛📶📶⬛⬛📶⬛⬛\n⬛⬛⬛⬛⬛⬛⬛⬛⬛\n⬛📶⬛📶📶📶📶📶⬛\n⬛⬛⬛⬛⬛⬛⬛⬛⬛ \n‎‎‎‎‎‎‎‎‎ \n \n My 🇵 🇮 🇳 🇬  Is : {ms} ms"
     )
+
+
+# ===============================================
+
+
+@catub.cat_cmd(
+    pattern="mping$",
+    command=("mping", plugin_category),
+    info={
+        "header": "Checks the latency of userbot from the server, with a media",
+        "option": "VARS to customize the texts of mping\n`PING_PICS` add mutiple telegraph media link separated by spaces.\n`PING_TEXT` Pre text i.e. before calculation ping.\n`PONG_TEXT` Post text i.e. the final message.\n`PING_MENTION` Custom mention line.\n`PING_PARTNER` Text after ping(that random number)",
+        "usage": "{tr}mping",
+    },
+)
+async def _(event):
+
+    "Shows ping with a given random media"
+
+    if event.fwd_from:
+
+        return
+
+    reply_to_id = await reply_id(event)
+
+    start = datetime.now()
+
+    cat = await edit_or_reply(event, f"{PING_TEXT}", "html")
+
+    end = datetime.now()
+
+    await cat.delete()
+
+    ms = (end - start).microseconds / 1000
+
+    PING_PIC = random.choice(PING_PICS)
+
+    if PING_PIC:
+
+        try:
+
+            while PING_PIC == "":
+
+                PING_PIC = random.choice(PING_PICS)
+
+        except IndexError:
+
+            pass
+
+            error = "fix"  # This line is just to prevent any NoneType error
+
+        caption = (
+            f"{PONG_TEXT}\n<code>{ms} {PING_PARTNER}</code>\n{PING_MENTION} {hmention}"
+        )
+
+        await event.client.send_file(
+            event.chat_id,
+            PING_PIC,
+            caption=caption,
+            parse_mode="html",
+            reply_to=reply_to_id,
+            link_preview=False,
+            allow_cache=True,
+        )
+
+
+# ==================================================================================================
